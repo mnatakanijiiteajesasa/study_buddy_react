9683fe6b9bdd8cdafe92c7ef1c75da8d4a58b2b0
mnatakanijiiteajesasa <156783223+mnatakanijiiteajesasa@users.noreply.github.com>
2024-06-05 22:47:38 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Navbar from './Navbar';
+import Body from './Body';
+
+function App() {
+  return (
+    <div className="App-header">
+      <Navbar/>
+      <div className="App-content">
+        <Body/>
+      </div>
+    </div>
+
+  );
+}
+
+export default App;
