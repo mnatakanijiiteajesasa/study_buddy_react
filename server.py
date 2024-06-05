654dc2c720dd199cff0c7a5bb5f6cbb3f5e8c0eb
mnatakanijiiteajesasa <156783223+mnatakanijiiteajesasa@users.noreply.github.com>
2024-06-05 18:47:41 +0300

    server.py

--- server.py
+++ server.py
@@ -2,25 +2,13 @@ import requests
 import pywhatkit as kit
 from flask import Flask, jsonify
 
-app = Flask(__name__)
-
-
-@app.route('/api/data', method=['GET'])
-def get_data():
-    data = {'message': 'Hello from backend!'}
-    return jsonify(data)
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
-
 # this file is going to house the entire back end for the website
 
 phone_number = input("Enter your phone number starting with your country code: \n")
 user_query = input("What unit are you interested in? ")
 time = input("When do you want to receive the message")
 
+#scraping open library for books
 
 def search_book_in_library(query):
 
