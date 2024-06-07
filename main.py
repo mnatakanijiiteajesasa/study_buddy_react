f6eed26921f483506cd22518a3b3f04b8a17a786
diggy <156783223+mnatakanijiiteajesasa@users.noreply.github.com>
2024-06-07 23:00:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
-soo empty
+import requests
+import time
+
+API_KEY = "UQWc4yHAdnwYsz9527TIxabPJueCNrXM"
+URL = "https://core.ac.uk/contact"
+
+user_request = input("Enter the subject of the cook, journal, article or research papers you are looking for: \n")
+
+header = {
+    'Authorisation': f'Bearer {API_KEY}',
+    'Accept': 'application/json'
+}
+
+params = {
+    'q': user_request,
+    'language': "en",
+    # 'journals': user_request,
+    'sort': 'relevance'
+}
+max_retries = 5
+retry_delay = 5
+for attempt in range(max_retries):
+    response = requests.get(url=URL, headers=header, params=params)
+    if response.status_code == 200:
+        data = response.json()
+        print(data)
+        break
+    elif response.status_code == 530:
+        print(f'Error {response.status_code}: Server Unavailable. Retrying in a {retry_delay}seconds')
+        time.sleep(retry_delay)
+    else:
+        print(f'Error: {response.status_code}')
+else:
+    print("Oops! Service not available")
+
