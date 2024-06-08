fb5afa6873fa507f2f14f49fa0c31b7b4dca6d52
diggy <156783223+mnatakanijiiteajesasa@users.noreply.github.com>
2024-06-08 18:33:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import requests
+from flask import Flask, render_template
+
+app = Flask(__name__)
 
 # Function for scraping the CORE API for journals, articles, and research works
 def search_core_api(journal_query):
@@ -35,6 +38,20 @@ def search_book_in_library(book_query):
 
     response = requests.get(url=open_library_url, params=params, headers=headers)
     print(response.json())
+    books = response.json()['docs']
+    book_covers = [
+        f"https://covers.openlibrary.org/b/id/{book['cover_i']}-L.jpg" for book in books if 'cover_i' in book
+    ]
+    return book_covers
+
+@app.route('/search')
+def search():
+    book_query = request.args.get('q', '')
+    book_covers = search_book_in_library(book_query)
+    return render_template('search_results.html', book_covers=book_covers)
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
 category = input("What do you want to search for? (Books or journals): ")
 # Function to decide which search to perform
