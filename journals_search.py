097b120e718830378fe651d0c1e8953e0f5ccdf0
diggy <156783223+mnatakanijiiteajesasa@users.noreply.github.com>
2024-06-27 14:56:34 +0300

    Update journals_search.py

--- journals_search.py
+++ journals_search.py
@@ -1,36 +1,37 @@
 import requests
-import time
+from flask import Flask
 
-API_KEY = "UQWc4yHAdnwYsz9527TIxabPJueCNrXM"
-URL = "https://core.ac.uk/contact"
+app = Flask(__name__)
 
-user_request = input("Enter the subject of the journal, article and research papers you are looking for: \n")
 
-header = {
-    'Authorisation': f'Bearer {API_KEY}',
-    'Accept': 'application/json'
-}
+@app.route('/search_core_api', methods=['GET'])
+def search_core(journal_query):
+    # Code for scraping the CORE API goes here
+    user_jar = input("Enter the name of the article, journal or research work you are searching for: \n")
 
-params ={
-    'q':user_request,
-    'language':"en",
-    'journals': user_request,
-    'sort': 'relevance'
-}
+    CORE_API_KEY = "UQWc4yHAdnwYsz9527TlxabPJueCNrXM"
+    CORE_URL = "https://api.core.ac.uk/v3/search/works"
 
-max_retries = 5
-retry_delay = 5
+    headers = {
+        'Authorization': f'Bearer {CORE_API_KEY}',
+        'Accept': 'application/json'
+    }
 
-for attempt in range(max_retries):
-    response = requests.get(url=URL, headers=header, params=params)
-    if response.status_code == 200:
-        data = response.json()
-        print(data)
-        break
-    elif response.status_code == 530:
-        print(f'Error {response.status_code}: Server Unavailable. Retrying in a {retry_delay}seconds')
-        time.sleep(retry_delay)
-    else:
-        print(f'Error: {response.status_code}')
-else:
-    print("Oops! Service not available")
+    params = {
+        'q': journal_query,
+        'language': "en",
+        'limit': '5',
+        'journals': journal_query,
+        'sort': 'relevance'
+    }
+
+    response = requests.get(url=CORE_URL, params=params, headers=headers)
+    journal_results = response.json()['docs']
+    return journal_results
+
+
+
+
+
+
+
